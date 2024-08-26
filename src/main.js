3ca438aa4f6d1194490cec72e7b03d7e88c70804
Max Froumentin <git@e97.me>
2024-08-26 22:47:05 +0100

    Put everything in index file again

--- src/main.js
+++ src/main.js
@@ -4,17 +4,27 @@ const $ = (id) => document.getElementById(id);
 
 const widgetValues = () => {
   const params = {};
-  ['margin', 'seed', 'shape1', 'shape2', 'nbNodes'].forEach(id => {
+  // Common params
+  ['margin', 'seed', 'shape1', 'shape2'].forEach(id => {
     params[id] = parseFloat($(id).value);
   });
   params['showGraph'] = $('showGraph').checked;
   params['graphType'] = $('graphType').value;
+
+  // Random only
+  params['nbNodes'] = parseInt($('nbNodes').value);
+
+  // Grid only
+  params['cells'] = parseInt($('cells').value);
+
+  // Polar only
+  params['cells'] = parseInt($('cells').value);
+  params['nbOrbits'] = parseInt($('nbOrbits').value);
+  params['nbNodesPerOrbit'] = parseInt($('nbNodesPerOrbit').value);
+
   return params;
 };
 
-$('showGraph').addEventListener('change', () => {
-    $('canvas').innerHTML = render(widgetValues());
-})
 
 const saveSvg = function() {
   const svgEl = $('svg-canvas');
@@ -33,17 +43,27 @@ const saveSvg = function() {
 
 // ============ Add event listeners ============
 
+$('showGraph').addEventListener('change', () => {
+  $('canvas').innerHTML = render(widgetValues());
+});
+
+$('graphType').addEventListener('change', () => {
+  $('canvas').innerHTML = render(widgetValues());
+});
+
+
+
+$('graphType').addEventListener('change', () => {
+  $('canvas').innerHTML = render(widgetValues());
+});
+
 $('saveSvg').addEventListener('click', saveSvg);
 
-['margin', 'seed', 'shape1', 'shape2', 'nbNodes'].forEach(id => {
+['margin', 'seed', 'shape1', 'shape2', 'nbNodes', 'cells', 'nbOrbits', 'nbNodesPerOrbit'].forEach(id => {
   $(id).addEventListener('change', event => {
     $(`${id}-value`).innerText = event.target.value;
     $('canvas').innerHTML = render(widgetValues());
   });
-});
-
-$('showGraph').addEventListener('change', event => {
-  $('canvas').innerHTML = render(widgetValues());
 });
 
 
