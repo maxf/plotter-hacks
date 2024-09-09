6c1c385c3b135d163b3781783437db1896c09a20
Max Froumentin <max@e97.me>
2024-09-09 20:05:57 +0100

    save/get state from QSP

--- src/main.js
+++ src/main.js
@@ -3,7 +3,28 @@ import { renderBoids } from './boids-plot';
 
 const $ = (id) => document.getElementById(id);
 
-const widgetValues = () => {
+
+
+const defaultParams = {
+  'plotType': 'Polar',
+  'margin': 100,
+  'seed': 100,
+  'shape1': 0.3,
+  'shape2': 0.3,
+  'showGraph': false,
+  'nbNodes': 4,
+  'cells': 4,
+  'perturbation': 0,
+  'nbOrbits': 3,
+  'nbNodesPerOrbit': 3,
+  'iterations': 100,
+  'nboids': 10,
+  'speedLimit': 30,
+  'cohesionForce': 0.5
+}
+
+
+const paramsFromWidgets = () => {
   const params = {};
   // Common params
   ['margin', 'seed', 'shape1', 'shape2'].forEach(id => {
@@ -29,8 +50,18 @@ const widgetValues = () => {
   ['iterations', 'nboids', 'speedLimit', 'cohesionForce'].forEach(id => {
     params[id] = parseFloat($(id).value);
   });
-  
+
   return params;
+};
+
+
+const paramsFromUrl = (defaults) => {
+  const params = new URLSearchParams(window.location.search);
+  const result = defaults;
+  for (const [key, value] of params) {
+    result[key] = isNaN(value) ? value : parseFloat(value);
+  }
+  return result;
 };
 
 
@@ -49,31 +80,38 @@ const saveSvg = function() {
   document.body.removeChild(downloadLink);
 }
 
+const paramsPerType = {
+  Random: ['showGraph', 'shape1', 'shape2', 'nbNodes'],
+  Grid: ['showGraph', 'shape1', 'shape2', 'cells', 'perturbation'],
+  Polar: ['showGraph', 'shape1', 'shape2', 'nbOrbits', 'nbNodesPerOrbit', 'perturbation'],
+  Boids: ['iterations', 'nboids', 'speedLimit', 'cohesionForce'],
+  common: ['seed', 'plotType', 'margin']
+};
 
 const activateControls = plotType => {
   const show = id => $(`${id}-control`).style.display = 'inline';
   document.querySelectorAll('.control').forEach(el => el.style.display = 'none');
   ['margin', 'seed'].forEach(show);
-  switch (plotType) {
-  case 'Random':
-    ['showGraph', 'shape1', 'shape2', 'nbNodes'].forEach(show);
-    break;
-  case 'Grid':
-    ['showGraph', 'shape1', 'shape2', 'cells', 'perturbation'].forEach(show);
-    break;
-  case 'Polar':
-    ['showGraph', 'shape1', 'shape2', 'nbOrbits', 'nbNodesPerOrbit', 'perturbation'].forEach(show);
-    break;
-  case 'Boids':
-    ['iterations', 'nboids', 'speedLimit', 'cohesionForce'].forEach(show);
-    break; 
-  }
+  paramsPerType[plotType].forEach(show);
 };
 
+const updateUrl = params => {
+  const url = new URL(window.location);
+  url.search = '';
+  const qsParams = paramsPerType.common.concat(paramsPerType[params.plotType]);
+  qsParams.forEach(key => {
+    url.searchParams.set(key, params[key]);
+  });
+  history.pushState(null, '', url);
+};
+
+// ========== Main rendering function ===========
 
 const render = params => {
   params.width ||= 800;
   params.height ||= 800;
+
+  updateUrl(params);
 
   const renderFn = (['Random', 'Grid', 'Polar'].includes(params.plotType))
         ? renderCeltic
@@ -85,12 +123,12 @@ const render = params => {
 // ============ Add event listeners ============
 
 $('showGraph').addEventListener('change', () => {
-  $('canvas').innerHTML = render(widgetValues());
+  $('canvas').innerHTML = render(paramsFromWidgets());
 });
 
 $('plotType').addEventListener('change', () => {
   activateControls($('plotType').value);
-  $('canvas').innerHTML = render(widgetValues());
+  $('canvas').innerHTML = render(paramsFromWidgets());
 });
 
 $('saveSvg').addEventListener('click', saveSvg);
@@ -102,12 +140,30 @@ $('saveSvg').addEventListener('click', saveSvg);
 ].forEach(id => {
   $(id).addEventListener('change', event => {
     $(`${id}-value`).innerText = event.target.value;
-    $('canvas').innerHTML = render(widgetValues());
+    $('canvas').innerHTML = render(paramsFromWidgets());
   });
 });
 
 
 // =========== First render =============
 
-activateControls($('plotType').value);
-$('canvas').innerHTML = render(widgetValues());
+// Fetch plot parameters from the query string
+const params = paramsFromUrl(defaultParams);
+
+// populate the form controls from params
+[
+  'margin', 'seed', 'shape1', 'shape2', 'nbNodes', 'cells', 'nbOrbits',
+  'nbNodesPerOrbit', 'perturbation', 'iterations', 'nboids', 'speedLimit',
+  'cohesionForce'
+].forEach(key => {
+  $(key).value = $(`${key}-value`).innerText = params[key];
+});
+
+$('showGraph').checked = params.showGraph;
+$('plotType').value = params.plotType;
+
+// only show the relevant ones
+activateControls(params.plotType);
+
+
+$('canvas').innerHTML = render(params);
