f7b6cda9f7bc6e1b533d0c49dd945e3f37c8b6b2
Max Froumentin <github@e97.me>
2025-03-21 15:40:01 +0000

    build

--- build/textorizer2-ww.js
+++ build/textorizer2-ww.js
@@ -337,73 +337,73 @@ var strokeGlyphs = {
 
 // src/textorizer2-ww.ts
 var Textorizer2 = class {
-  #image;
-  #cutoff;
-  #text;
-  #fontSize;
-  #textIndex;
-  #nbLayers;
-  #lineHeight;
+  image;
+  cutoff;
+  text;
+  fontSize;
+  textIndex;
+  nbLayers;
+  lineHeight;
+  //------------------------------------------------------------------------
   constructor(params, imageData) {
-    this.#image = new Pixmap(imageData);
-    this.#text = params.text.replace(/\n+/g, " - ");
-    this.#cutoff = params.cutoff;
-    this.#fontSize = params.fontSize;
-    this.#textIndex = 0;
-    this.#nbLayers = params.nbLayers;
-    this.#lineHeight = params.lineHeight;
+    this.image = new Pixmap(imageData);
+    this.text = params.text.replace(/\n+/g, " - ");
+    this.cutoff = params.cutoff;
+    this.fontSize = params.fontSize;
+    this.textIndex = 0;
+    this.nbLayers = params.nbLayers;
+    this.lineHeight = params.lineHeight;
   }
-  #toSvgScanLine(row, cutoff, dx = 0, dy = 0) {
-    const w = this.#image.width;
+  //------------------------------------------------------------------------
+  toSvgScanLine(row, cutoff, dx = 0, dy = 0) {
+    const w = this.image.width;
     let x = 0;
     const svg = [];
+    const scale = this.fontSize * 1e-3;
     while (x <= w) {
-      const imageLevel = this.#image.brightnessAt(x, row);
+      const imageLevel = this.image.brightnessAt(x, row);
       let glyph, glyphInfo;
       if (imageLevel < cutoff) {
-        glyph = this.#text[this.#textIndex];
-        glyphInfo = strokeGlyphs[glyph];
-        if (glyph !== " ") {
-          const scale = this.#fontSize * 1e-3;
+        glyph = this.text[this.textIndex];
+        glyphInfo = strokeGlyphs[glyph] || strokeGlyphs["*"];
+        if (glyphInfo && glyph !== " ") {
           svg.push(`<path vector-effect="non-scaling-stroke" d="${glyphInfo[2]}" transform="translate(${x + dx}, ${row + dy}) scale(${scale},${-scale})" />`);
         }
-        this.#textIndex = (this.#textIndex + 1) % this.#text.length;
+        this.textIndex = (this.textIndex + 1) % this.text.length;
       } else {
         glyph = " ";
         glyphInfo = strokeGlyphs[" "];
       }
-      x += glyphInfo[1] * this.#fontSize * 1e-3;
+      x += glyphInfo[1] * scale;
     }
     const content = svg.join("");
     return content === "" ? "" : `<g class="scan-line" data-row="${row}">${content}</g>`;
   }
-  #toSvgScan(cutoff, dx = 0, dy = 0) {
-    const h = this.#image.height;
+  //------------------------------------------------------------------------
+  toSvgScan(cutoff, dx = 0, dy = 0) {
+    const h = this.image.height;
     const svg = [];
-    for (let row = this.#fontSize; row <= h; row += this.#fontSize) {
-      svg.push(this.#toSvgScanLine(row, cutoff, dx, dy));
+    for (let row = this.fontSize; row <= h; row += this.fontSize) {
+      svg.push(this.toSvgScanLine(row, cutoff, dx, dy));
     }
     const scan = svg.join("");
     return scan === "" ? "" : `<g id="scan">${scan}</g>`;
   }
+  //------------------------------------------------------------------------
   toSvg() {
-    const w = this.#image.width;
-    const h = this.#image.height;
+    const w = this.image.width;
+    const h = this.image.height;
     const svg = [];
     svg.push(`<svg xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape" id="svg-canvas" height="100vh" viewBox="0 0 ${w} ${h}">`);
-    svg.push(`
-    <!-- <rect x="0" y="0" width="${w}" height="${h}" style="stroke: black; stroke-width: 1; fill: none"/> -->
-    <!-- <g style="stroke: black; stroke-width: 1; fill: none;"> -->
-    `);
-    const step = this.#cutoff / this.#nbLayers;
-    for (let c = this.#nbLayers; c >= 1; c--) {
+    const step = this.cutoff / this.nbLayers;
+    for (let c = this.nbLayers; c >= 1; c--) {
       const x = c * step;
-      const dy = c === this.#nbLayers ? 0 : Math.random() * this.#lineHeight * 5;
+      const dy = c === this.nbLayers ? 0 : Math.random() * this.lineHeight * 5;
       svg.push(`<g id="layer${c}" inkscape:groupmode="layer" inkscape:label="${c}" style="stroke: black; stroke-width: 1; fill: none;">`);
-      svg.push(this.#toSvgScan(x, 0, dy));
+      svg.push(this.toSvgScan(x, 0, dy));
       svg.push(`</g>`);
     }
-    svg.push("<!--</g>--></svg>");
+    svg.push("</svg>");
     return svg.join("");
   }
 };
