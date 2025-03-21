4958ca8ebee6ca91845c57cbd1b573ad8f94441d
Max Froumentin <github@e97.me>
2025-03-21 14:48:17 +0000

    Reformat using gptel-aibo

--- src/textorizer2-ww.ts
+++ src/textorizer2-ww.ts
@@ -13,84 +13,89 @@ type Params = {
 };
 
 class Textorizer2 {
-  #image: Pixmap;
-  #cutoff: number;
-  #text: string;
-  #fontSize: number;
-  #textIndex: number;
-  #nbLayers: number;
-  #lineHeight: number;
+  private image: Pixmap;
+  private cutoff: number;
+  private text: string;
+  private fontSize: number;
+  private textIndex: number;
+  private nbLayers: number;
+  private lineHeight: number;
+
+  //------------------------------------------------------------------------
 
   constructor(params: Params, imageData: ImageData) {
-    this.#image = new Pixmap(imageData);
-    this.#text = params.text.replace(/\n+/g, ' - ');
-    this.#cutoff = params.cutoff;
-    this.#fontSize = params.fontSize;
-    this.#textIndex = 0;
-    this.#nbLayers = params.nbLayers;
-    this.#lineHeight = params.lineHeight;
+    this.image = new Pixmap(imageData);
+    this.text = params.text.replace(/\n+/g, ' - ');
+    this.cutoff = params.cutoff;
+    this.fontSize = params.fontSize;
+    this.textIndex = 0;
+    this.nbLayers = params.nbLayers;
+    this.lineHeight = params.lineHeight;
   }
 
-  #toSvgScanLine(row: number, cutoff: number, dx: number = 0, dy: number = 0): string {
-    const w = this.#image.width;
+  //------------------------------------------------------------------------
+
+  private toSvgScanLine(row: number, cutoff: number, dx: number = 0, dy: number = 0): string {
+    const w = this.image.width;
     let x = 0;
     const svg = [];
     while(x <= w) {
-      const imageLevel = this.#image.brightnessAt(x, row);
+      const imageLevel = this.image.brightnessAt(x, row);
       let glyph, glyphInfo;
       if (imageLevel < cutoff) {
-        glyph = this.#text[this.#textIndex];
+        glyph = this.text[this.textIndex];
         glyphInfo = strokeGlyphs[glyph];
         if (glyph !== ' ') {
-          const scale = this.#fontSize * 0.001;
+          const scale = this.fontSize * 0.001;
           svg.push(`<path vector-effect="non-scaling-stroke" d="${glyphInfo[2]}" transform="translate(${x+dx}, ${row+dy}) scale(${scale},${-scale})" />`);
         }
-        this.#textIndex = (this.#textIndex + 1) % this.#text.length;
+        this.textIndex = (this.textIndex + 1) % this.text.length;
       } else {
         glyph = ' ';
         glyphInfo = strokeGlyphs[' '];
       }
-      x += glyphInfo[1]*this.#fontSize * 0.001;
+      x += glyphInfo[1]*this.fontSize * 0.001;
     }
     const content = svg.join('');
     return content === '' ? '' : `<g class="scan-line" data-row="${row}">${content}</g>`;
   }
 
-  #toSvgScan(cutoff: number, dx: number = 0, dy: number = 0): string {
-    const h = this.#image.height;
+  //------------------------------------------------------------------------
+
+  private toSvgScan(cutoff: number, dx: number = 0, dy: number = 0): string {
+    const h = this.image.height;
     const svg = [];
-    for (let row = this.#fontSize; row <= h; row += this.#fontSize) {
-      svg.push(this.#toSvgScanLine(row, cutoff, dx, dy));
+    for (let row = this.fontSize; row <= h; row += this.fontSize) {
+      svg.push(this.toSvgScanLine(row, cutoff, dx, dy));
     }
     const scan = svg.join('');
     return scan === '' ? '' : `<g id="scan">${scan}</g>`;
   }
 
+  //------------------------------------------------------------------------
+
   toSvg(): string {
-    const w = this.#image.width;
-    const h = this.#image.height;
+    const w = this.image.width;
+    const h = this.image.height;
     const svg = [];
 
     svg.push(`<svg xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape" id="svg-canvas" height="100vh" viewBox="0 0 ${w} ${h}">`);
-    svg.push(`
-    <!-- <rect x="0" y="0" width="${w}" height="${h}" style="stroke: black; stroke-width: 1; fill: none"/> -->
-    <!-- <g style="stroke: black; stroke-width: 1; fill: none;"> -->
-    `);
 
-    const step = this.#cutoff / this.#nbLayers;
-      //for (let c = 1; c <= this.#nbLayers; c++) {
-    for (let c = this.#nbLayers; c >= 1; c--) {
+    const step = this.cutoff / this.nbLayers;
+    for (let c = this.nbLayers; c >= 1; c--) {
       const x = c*step;
-      const dy = c === this.#nbLayers ? 0 : Math.random() *this.#lineHeight*5;
+      const dy = c === this.nbLayers ? 0 : Math.random() *this.lineHeight*5;
       svg.push(`<g id="layer${c}" inkscape:groupmode="layer" inkscape:label="${c}" style="stroke: black; stroke-width: 1; fill: none;">`);
-      svg.push(this.#toSvgScan(x, 0, dy));
+      svg.push(this.toSvgScan(x, 0, dy));
       svg.push(`</g>`);
     }
 
-    svg.push('<!--</g>--></svg>');
+    svg.push('</svg>');
     return svg.join('');
   }
 }
+
+//------------------------------------------------------------------------
 
 onmessage = function(e) {
   const { params, imageData } = e.data;
